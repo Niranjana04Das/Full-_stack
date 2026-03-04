0148b78ebde573efef63553224820bab897bd55c
Niranjana Das <niranjana@gmail.com>
2026-03-04 11:58:17 +0530

    completed 3.7-3.8 in part3

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
-
+const morgan = require('morgan')
 app.use(express.json())
+morgan.token('body', function (req) {
+  return JSON.stringify(req.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 const persons = [
   { id: "1", name: "Arto Hellas", number: "040-123456" },
